cb39077bc8bce667f2d6c2d4252652497f2cf765
Stew <stewforani@gmail.com>
2023-05-17 17:20:28 +0800

    add asm test

--- app-plugin/src/main/java/com/life/jv/ActivityLifeMethodVisitor.java
+++ app-plugin/src/main/java/com/life/jv/ActivityLifeMethodVisitor.java
@@ -26,4 +26,17 @@ public class ActivityLifeMethodVisitor extends MethodVisitor {
       mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Class", "getName", "()Ljava/lang/String;", false);
       mv.visitMethodInsn(Opcodes.INVOKESTATIC, "com/stew/androidtest/util/AppLogUtil", "addLifeLog", "(Ljava/lang/String;)V", false);
    }
+
+   @Override
+   public void visitInsn(int opcode) {
+      //方法执行后插入
+      if (opcode == Opcodes.RETURN) {
+//         mv.visitLdcInsn("tag");
+//         mv.visitLdcInsn("onCreate end");
+//         mv.visitMethodInsn(Opcodes.INVOKESTATIC, "android/util/Log", "d", "(Ljava/lang/String;Ljava/lang/String;)I", false);
+//         mv.visitInsn(Opcodes.POP);
+      }
+      super.visitInsn(opcode);
+
+   }
 }
